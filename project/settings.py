8a91fec8e39e98c349698a41331e6be1b1d0b686
jgelli <juan_gelli@hotmail.com>
2023-02-15 19:15:45 -0300

    :bug: fix allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')  # noqa: E501
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = ['https://*', ]
+ALLOWED_HOSTS = ['http://*', 'https://*', ]
 CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS.copy()
 
 
